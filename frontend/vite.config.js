0fde88a19a27fbfab05952e98a142426de5e1afe
hangie921 <hangie921@gmail.com>
2025-03-17 13:01:00 +0800

    refactor: [FE] adjust vite.config.js to import _Variables.scss in *.scss

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: ["707d-2001-b400-e387-ccb1-1876-70c5-e595-9cd4.ngrok-free.app"],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "Variables" as *;`
+      }
+    }
   }
 })
