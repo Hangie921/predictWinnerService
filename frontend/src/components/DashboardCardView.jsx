d213424e359ff9f69846167c703dbdea39bca0b0
hangie921 <hangie921@gmail.com>
2025-03-20 12:37:37 +0800

    feat: [FE] adjust styles to make Dashboard card view responsive

--- frontend/src/components/DashboardCardView.jsx
+++ frontend/src/components/DashboardCardView.jsx
@@ -1,32 +1,16 @@
 import Card from "./Card";
 
 function DashboardCardView({ cards }) {
-    const getCardRows = function () {
-        let tmp = Array.from(cards)
-        let ret = []
-        while (tmp.length) ret.push(tmp.splice(0, 3))
-        return ret
-    }
-
     return (
         <div className="cards-container">
-            {
-                getCardRows().map((row, rIdx) => {
-                    const rowClassName = `card-row ${row.length < 3 ? 'card-row-partial' : ''}`
-                    return (
-                        <div className={rowClassName} key={rIdx}>
-                            {row.map((card, cIdx) => (
-                                <div className="card-wrapper" key={cIdx}>
-                                    <Card 
-                                        index={cIdx + 1}
-                                        data={card}
-                                    />
-                                </div>
-                            ))}
-                        </div>
-                    )
-                })
-            }
+            {cards.map((card, idx) => (
+                <div className="card-wrapper" key={card.gameID || idx}>
+                    <Card 
+                        index={idx + 1}
+                        data={card}
+                    />
+                </div>
+            ))}
         </div>
     )
 }
